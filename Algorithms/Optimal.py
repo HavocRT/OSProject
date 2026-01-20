34f37516ef8ec34fd363560ae62d72ee24c394f9
AnushJJ <anushjohn2005@gmail.com>
2026-01-20 19:41:14 +0530

    Create Optimal.py
    created optimal page replacement algorithm

---
+++ Algorithms/Optimal.py
@@ -0,0 +1,25 @@
+from Algorithms.BaseReplacement import PageReplacementAlgorithm
+
+class Optimal(PageReplacementAlgorithm):
+    def __init__(self, frameLimit, referenceString):
+        super().__init__(frameLimit)
+        self.referenceString = referenceString
+
+    def AccessPage(self, page, time):
+        if page not in self.frames:
+            self.pageFaults += 1
+
+            if len(self.frames) == self.frameLimit:
+                future = self.referenceString[time + 1:]
+                distances = {}
+
+                for p in self.frames:
+                    if p in future:
+                        distances[p] = future.index(p)
+                    else:
+                        distances[p] = float('inf')
+
+                victim = max(distances, key=distances.get)
+                self.frames.remove(victim)
+
+            self.frames.append(page)
