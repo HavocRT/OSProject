eaa230edd6e98afb608950d08ba925e3a8e2bb9b
AnushJJ <anushjohn2005@gmail.com>
2026-01-20 19:55:18 +0530

    Update Main.py
    updated main to test out ipra and tlb

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
-from Algorithms.FIFO import FIFO
-from Algorithms.LRU import LRU
-from Algorithms.Optimal import Optimal
+from Algorithms.IPRA import IPRA
+from Algorithms.TLB import TLB
+from Simulator.MemorySimulator import MemorySimulator
 
-referenceString = [1, 2, 3, 4, 1, 2, 5]
-frameLimit = 3
+ipra = IPRA(frames=3, window_size=4)
+tlb = TLB(size=2)
 
-algorithm = LRU(frameLimit)
+sim = MemorySimulator(ipra, tlb)
 
-time = 0
-for page in referenceString:
-    algorithm.AccessPage(page, time)
-    time += 1
+reference_string = [1, 2, 3, 1, 4, 1, 2, 5]
 
-print("Page Faults:", algorithm.GetPageFaults())
+for page in reference_string:
+    sim.AccessPage(page)
+
+print("Page Faults:", ipra.PageFaults)
+print("TLB Hit Rate:", tlb.HitRate())
