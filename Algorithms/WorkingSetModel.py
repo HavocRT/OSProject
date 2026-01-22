affc7a808b8a3909714475e4a7fb8325e9554a19
AnushJJ <anushjohn2005@gmail.com>
2026-01-22 20:12:42 +0530

    Update WorkingSetModel.py
    reworked the working set model

--- Algorithms/WorkingSetModel.py
+++ Algorithms/WorkingSetModel.py
@@ -1,20 +1,25 @@
 class WorkingSetModel:
-    def __init__(self, windowSize):
-        self.windowSize = windowSize
+    def __init__(self, tau):
+        self.tau = tau
         self.accessHistory = []
-
+    
     def recordAccess(self, page, time):
-        self.accessHistory.append((time, page))
-        self.accessHistory = [
-            (t, p) for (t, p) in self.accessHistory
-            if time - t <= self.windowSize
-        ]
-
-    def getWorkingSet(self):
-        return set(p for (_, p) in self.accessHistory)
-
-    def workingSetSize(self):
-        return len(self.getWorkingSet())
-
-    def isThrashing(self, frameCount):
-        return self.workingSetSize() > frameCount
+        self.accessHistory.append((page, time))
+    
+    def getWorkingSet(self, currentTime):
+        workingSet = set()
+        windowStart = max(0, currentTime - self.tau)
+        
+        for page, time in reversed(self.accessHistory):
+            if time >= windowStart and time <= currentTime:
+                workingSet.add(page)
+            elif time < windowStart:
+                break
+        
+        return workingSet
+    
+    def getWorkingSetSize(self, currentTime):
+        return len(self.getWorkingSet(currentTime))
+    
+    def clear(self):
+        self.accessHistory = []
